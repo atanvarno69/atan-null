74cda8214f6283bda7d863665248ca2028ad42aa
atanvarno69 <atanvarno69@gmail.com>
2023-12-29 14:15:07 +0000

    Initial commit

---
+++ data.lua
@@ -0,0 +1,11 @@
+data:extend({
+    {
+        type = "item",
+        name = "atan-null",
+        order = "zzz",
+        stack_size = 1,
+        icon = "__core__/graphics/no-recipe.png",
+        icon_size = 101,
+        subgroup = "raw-resource",
+    },
+})
